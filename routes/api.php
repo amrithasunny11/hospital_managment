79dd594df5b05ac03202727273db16685d1dee1b
anamikaammu <30740684+amrithasunny11@users.noreply.github.com>
2024-12-30 12:24:52 +0530

    Permission restriction added

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\AuthController;
 
 use App\Http\Controllers\GroupController;
 
+use App\Http\Middleware\CheckUserRole;
+
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
@@ -16,19 +19,22 @@ Route::post('/login', [AuthController::class, 'login']);
 
 
 
-Route::middleware('auth:sanctum')->group(function () {
-    // POST /groups - Create a new group
-    Route::post('/groups', [GroupController::class, 'store']);
+Route::middleware('auth:sanctum')->group(function () { 
 
     // GET /groups - Get all groups (hierarchical structure)
     Route::get('/groups', [GroupController::class, 'index']);
 
     // GET /groups/{id} - Get a specific group by ID
     Route::get('/groups/{id}', [GroupController::class, 'show']);
+ 
 
-    // PUT /groups/{id} - Update a group
-    Route::put('/groups/{id}', [GroupController::class, 'update']);
-
-    // DELETE /groups/{id} - Delete a group
-    Route::delete('/groups/{id}', [GroupController::class, 'destroy']);
+    // Routes restricted to admin role
+    Route::middleware([CheckUserRole::class . ':admin'])->group(function () {
+        // POST /groups - Create a new group
+        Route::post('/groups', [GroupController::class, 'store']);
+        // PUT /groups/{id} - Update a group
+        Route::put('/groups/{id}', [GroupController::class, 'update']);
+        // DELETE /groups/{id} - Delete a group
+        Route::delete('/groups/{id}', [GroupController::class, 'destroy']);
+    });
 });
